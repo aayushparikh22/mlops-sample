615a83967d93d8ffc3023d200564039606f6734c
Aayush Parikh <aayushparikh0000@gmail.com>
2025-05-06 23:52:12 +0530

    adding new values

--- src/infer.py
+++ src/infer.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def infer():
     model = joblib.load("model/model.pkl")
-    sample = pd.DataFrame({"feature1": [0.5], "feature2": [-0.1]})
+    sample = pd.DataFrame({"feature1": [-0.42845], "feature2": [-2.375]})
     prediction = model.predict(sample)
     print(f"Prediction for {sample.values.tolist()[0]}: {prediction[0]}")
 
